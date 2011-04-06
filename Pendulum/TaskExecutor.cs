31e4624c69a1a7cd0344d8eee3159c58ad9e91e1
awhatley <aaron.whatley@gmail.com>
2011-04-06 13:48:05 -0500

    Added task executor

---
+++ Pendulum/TaskExecutor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pendulum
+{
+    public class TaskExecutor
+    {
+        public void Execute(ScheduledTask task, ThreadController controller)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
